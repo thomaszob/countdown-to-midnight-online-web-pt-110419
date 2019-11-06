6135e643a46028d7c985ff83c21572da406385cf
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-11-06 05:21:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,4 +8,12 @@ def countdown(num)
     return "HAPPY NEW YEAR!"
 end
 
+def coountdown_with_sleep(num)
+  while num > 0 
+    puts "#{num} SECOND(S)"
+    num -= 1 
+    sleep(num)
+  end 
+    return "HAPPY NEW YEAR!"
+end 
 
